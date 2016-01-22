403f9a9dac3340dd46638d10a97b08b883bcc6c5
Hurter Jonathan <jonathan@hurter.fr>
2016-01-22 15:26:16 +0100

    Access Manager

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
+import manager.AccessManager;
 import ui.UIManager;
 
 
 public class Main {
 	public static void main(String[] args) {
-		UIManager.log("Hello World");
-		UIManager.setProgress(0.);
-		UIManager.setProgress(50.2);
-		UIManager.setProgress(100);
-		UIManager.log("DONE !");
+		testAccessManager();
+	}
+	
+	public static void testAccessManager(){
+		(new Thread() {
+			public void run() {
+				AccessManager.accessFile("test.txt");
+				UIManager.log("TH1 : Lock");
+
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				AccessManager.doneWithFile("test.txt");
+				UIManager.log("TH1 : Unlock");
+			}
+		}).start();
+		(new Thread() {
+			public void run() {
+				AccessManager.accessFile("test2.txt");
+				UIManager.log("TH2 : Lock");
+
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				AccessManager.doneWithFile("test2.txt");
+				UIManager.log("TH2 : Unlock");
+
+			}
+		}).start();
 	}
 
 }
