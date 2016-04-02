7dda6009d752ce8ec2d33657c6b1cd3fb9697b36
Hurter Jonathan <jonathan@hurter.fr>
2016-04-02 16:57:44 +0200

    Ajout du merge des fichiers excels.

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,22 @@
-import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.util.Scanner;
-
-import manager.AccessManager;
-import excel.*;
-import java.util.Scanner;
-import Parser.*;
-import Bdd.Bdd;
-
-import io.Net;
-import exceptions.CharInvalideException;
-import exceptions.ScannerNullException;
-import ui.UIManager;
 import java.util.ArrayList;
+
+import configuration.Configuration;
+
+import excel.ExcelManager;
 import manager.ThreadManager;
 import tree.*;
-
-
-//115.5 fich/heure
+import ui.UIManager;
 
 public class Main {
 
-	public static void main(String[] args) throws CharInvalideException, ScannerNullException {
+	public static void main(String[] args){
+		Tree plop = new Tree();
+		plop=TreeManager.constree();
 		
-		/*
-		//exemple d'utilisation du scanneur
-		Scanner sc = Net.getUrl("http://jonathancrabtree.github.io/Circleator/tutorials/gb_annotation/L42023.1.gb");
-		while(sc.hasNext())
-		{
-			UIManager.log(sc.next());
-		}
-		*/
-
-		//parsing
-		Bdd base = new Bdd("path");
+		ThreadManager.start(plop, new ArrayList<String>());
 		
-		//Scanner scanneur = Net.getUrl("http://jonathancrabtree.github.io/Circleator/tutorials/gb_annotation/L42023.1.gb");
-		//Scanner scanneur = Net.getUrl("file:///home/nestor/Cours/2A/bioinformatique/sequence2.gb");
-		Scanner scanneur = Net.getUrl("file:///home/micka/Bureau/Cours/3/BioInfo/sequence/sequence2.gb");
-		Parser parseur = new Parser(base,scanneur);
-		parseur.parse();
-				
-		//affichage du contenus de la bdd
-		//System.out.println("nbr_tri "+base.get_nb_trinucleotides());
-		//System.out.println("nbr_di "+base.get_nb_dinucleotides());
-		//System.out.println("nbr_cds "+base.get_nb_CDS());
-		//System.out.println("nbr_cds_nt "+base.get_nb_CDS_non_traites());
-		//System.out.println(base.get_tableauxnucleotides_string());
-		
-		String[] plop = new String[4];
-		plop [0] = "kingdom";
-		plop [1] = "groupe";
-		plop [2] = "subgroup";
-		plop [3] = "organisme";
-		ExcelWriter.writer("arbo/patate/souspatate/plop2/tortue/tortue.xls",plop, base);
-		
-
-		scanneur = Net.getUrl("file:///home/micka/Bureau/Cours/3/BioInfo/sequence/sequence3.gb");
-		//scanneur = Net.getUrl("file:///home/nestor/Cours/2A/bioinformatique/sequence3.gb");
-
-		parseur = new Parser(base,scanneur);
-		parseur.parse();
-		plop = new String[4];
-		plop [0] = "kingdom";
-		plop [1] = "groupe";
-		plop [2] = "subgroup";
-		plop [3] = "organisme";
-		ExcelWriter.writer("arbo/patate/souspatate/plop2/tortue2/tortue2.xls",plop, base);
-		
-		ExcelManager.fusionExcels("arbo");
-
+		UIManager.log("Creating excel files...");
+		ExcelManager.fusionExcels(Configuration.BASE_FOLDER);
 
 	}
 
