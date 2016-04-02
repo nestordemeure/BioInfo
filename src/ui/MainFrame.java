19a63713c484987dedc65661ccf667f8afeb3b0a
Hurter Jonathan <jonathan@hurter.fr>
2016-04-02 20:13:34 +0200

    Log scrollable dans le bon sens.

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -4,7 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Frame;
 
 import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.text.DefaultCaret;
 
 public class MainFrame extends Frame {
 	
@@ -12,17 +14,23 @@ public class MainFrame extends Frame {
 	
 	private JProgressBar progress;
 	private JTextArea logger;
+	private JScrollPane scroll;
 	
 	public MainFrame(){
 		this.setLayout(new BorderLayout());
 		logger = new JTextArea();
+		DefaultCaret c = (DefaultCaret) logger.getCaret();
+		c.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		
+		scroll = new JScrollPane(logger);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		progress = new JProgressBar();
 		progress.setDoubleBuffered(true);
 		progress.setStringPainted(true);
 
 		
-		this.add(logger,  BorderLayout.CENTER);
+		this.add(scroll,  BorderLayout.CENTER);
 		this.add(progress,BorderLayout.PAGE_END);
 		
 		setTitle("BioInfo : Main");
@@ -31,7 +39,7 @@ public class MainFrame extends Frame {
 	}
 	
 	public void log(String msg){
-		this.logger.insert(msg + "\n",0);
+		this.logger.insert(msg + "\n",this.logger.getText().length());
 	}
 	
 	public void setMaxProgress(int n){
