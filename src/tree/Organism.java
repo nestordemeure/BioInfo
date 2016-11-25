6e5a4e4d84e76c54596a264ba9e35e1251ec7e5e
Jonathan Hurter <jonathan@hurter.fr>
2016-11-25 12:47:01 +0100

    Add fields to organism

--- src/tree/Organism.java
+++ src/tree/Organism.java
@@ -1,14 +1,21 @@
 package tree;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Organism {
+public class Organism implements Serializable {
+	private static final long serialVersionUID = -2867789287775171672L;
 	private String kingdom;
 	private String group;
 	private String subgroup;
 	private String name;
 	private String bioproject;
+	private String taxonomy;
+	private String accession;
 	private Map<String, String> replicons;
 	
 	public Organism(String kingdom, String group, String subgroup, String name, String bioproject){
@@ -17,6 +24,10 @@ public class Organism {
 		this.subgroup = subgroup;
 		this.name = name;
 		this.bioproject = bioproject;
+		this.replicons = new HashMap<String, String>();
+	}
+	
+	public Organism(){
 		this.replicons = new HashMap<String, String>();
 	}
 	
@@ -59,11 +70,100 @@ public class Organism {
 
 	@Override
 	public String toString(){
+		
 		String str = this.kingdom+"/"+this.group+"/"+this.subgroup+"/"+this.name+"("+this.bioproject+")";
 		str += "\nReplicons :";
 		for(String name : this.replicons.keySet()){
 			str += "\n - "+name+" - "+this.replicons.get(name);
 		}
 		return str;
+	}
+	
+	private void readObject(ObjectInputStream inputstream) throws IOException, ClassNotFoundException 
+	{
+		kingdom = (String) inputstream.readObject();
+		group = (String) inputstream.readObject();
+		subgroup = (String) inputstream.readObject();
+		name = (String) inputstream.readObject();
+		bioproject = (String) inputstream.readObject();
+		accession = (String) inputstream.readObject();
+		taxonomy = (String) inputstream.readObject();
+		replicons = (HashMap<String,String>)inputstream.readObject();
+	}
+
+	private void writeObject(ObjectOutputStream outputstream) throws IOException
+	{
+		outputstream.writeObject(kingdom);
+		outputstream.writeObject(group);
+		outputstream.writeObject(subgroup);
+		outputstream.writeObject(name);
+		outputstream.writeObject(bioproject);
+		outputstream.writeObject(accession);
+		outputstream.writeObject(taxonomy);
+		outputstream.writeObject(replicons);
+	}
+
+	public String getKingdom() {
+		return kingdom;
+	}
+
+	public void setKingdom(String kingdom) {
+		this.kingdom = kingdom;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getSubgroup() {
+		return subgroup;
+	}
+
+	public void setSubgroup(String subgroup) {
+		this.subgroup = subgroup;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBioproject() {
+		return bioproject;
+	}
+
+	public void setBioproject(String bioproject) {
+		this.bioproject = bioproject;
+	}
+
+	public String getTaxonomy() {
+		return taxonomy;
+	}
+
+	public void setTaxonomy(String taxonomy) {
+		this.taxonomy = taxonomy;
+	}
+
+	public String getAccession() {
+		return accession;
+	}
+
+	public void setAccession(String accession) {
+		this.accession = accession;
+	}
+
+	public Map<String, String> getReplicons() {
+		return replicons;
+	}
+
+	public void setReplicons(Map<String, String> replicons) {
+		this.replicons = replicons;
 	}
 }
